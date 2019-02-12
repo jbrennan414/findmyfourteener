2d221d21c6f5a0ea7037ca51a0a1e36aa789f1ff
John Brennan <jpb55116@gmail.com>
2019-02-11 21:48:58 -0700

    passes data properly

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import Header from './header';
 import Myoptions from './myoptions';
 import Mypreferences from './mypreferences';
 
-class App extends Component {
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        data: "Default parent state"
+    };
+    
+    this.childHandler = this.childHandler.bind(this)
+  }
+
+  childHandler(dataFromChild) {
+      console.log('%cPrevious Parent State: ' + JSON.stringify(this.state), "color:orange");
+      this.setState({
+          data: dataFromChild
+      },() => console.log('Updated Parent State:', this.state));
+  }
+
   render() {
-    return (
-      <div className="App">
-        <Header />
-        <Mypreferences />
-        <Myoptions />
-      
-      
-      </div>
-    )
+
+    console.log('@@@@@@@ this.state', this.state);
+      return (
+        <div>
+          <Header />
+          <Myoptions action={this.childHandler} />
+        </div>
+      )
   }
 }
 
